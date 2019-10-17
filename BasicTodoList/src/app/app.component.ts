e5b17af7bd4f26293dc6f0fa0bc23734112a8b7e
RenaldoG <rgeldenhuis@gmail.com>
2019-10-17 15:47:04 +0200

    BasicTodoList
    TodoList

---
+++ BasicTodoList/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // define string property
+  name:string = 'Brad';
+
+  surname:string = 'Brady';
+
+  constructor() {
+    this.changeName('John');
+  }
+
+  changeName(name:string):void {
+    this.name = name;
+  }
+
+}
